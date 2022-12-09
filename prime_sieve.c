c00565978fa4f480c244021875997f2f09c27034
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2022-12-09 14:20:40 +0100

    Create prime_sieve.c

---
+++ prime_sieve.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/* This is a simple 'is it prime' tester that I'm going to use to try out sonarlint, sonarQube and SonarCloud a bit
+#
+# I'm not going to try and make this clever initially
+#
+# Future additions:
+#
+#   To test N:
+#   - Only test whether the prime numbers up to sqrt(N) divide into N without remainder
+#   - Use previously saved results to short circuit the test (for now I'll just test as if we have no known data)
+# 
+#   Save state between runs:
+#   - Save the list of primes you know about to disk for future runs
+# 
+#   Other extensions:
+#   - Ask user for N to test
+#   - Go and look up how people do this for real
+*/
+
+int is_prime(int number_to_test)
+{
+    int false = 0;
+    int true = 1;
+    int prime = true;
+    int always_true = true;
+    int max_test = number_to_test/2;
+
+    int i = 2;
+        
+    while ((i <= max_test) && (prime == true) && always_true) // interestingly, SonarLint doesn't catch use of & True here
+    {
+        if( number_to_test % i == 0)
+        {
+            prime = false;
+        }
+
+        i++;
+    }
+    
+    return(prime);
+
+}
+
+int main()
+{
+    // N to test
+    int N = 50;
+
+    for(int i = 2; i<N; i++)
+    {
+        if(is_prime(N))
+        {
+            printf("%d is prime", N);
+        }
+    }
+
+    return(0);
+}
+
+   
+
