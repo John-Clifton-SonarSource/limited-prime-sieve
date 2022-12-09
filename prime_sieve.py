c3c1d5af9bd83c29060070cd1f1902d4722b1aa8
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2022-12-09 11:33:01 +0100

    Create prime_sieve.py

---
+++ prime_sieve.py
@@ -0,0 +1,43 @@
+# This is a simple 'is it prime' tester that I'm going to use to try out sonarlint, sonarQube and SonarCloud a bit
+#
+# I'm not going to try and make this clever initially
+#
+# Future additions:
+#
+#   To test N:
+#   - Only test whether the prime numbers up to sqrt(N) divide into N without remainder
+#   - Use previously saved results to short circuit the test (for now I'll just test as if we have no known data)
+# 
+#   Save state between runs:
+#   - Save the list of primes you know about to disk for future runs
+# 
+#   Other extensions:
+#   - Ask user for N to test
+#   - Go and look up how people do this for real
+
+import math as m
+
+
+def is_prime(number):
+
+    prime = True
+    always_true = True
+    i = 2
+    max_test = m.sqrt(number)
+
+    if always_true != True:
+        print("This should never show - its just here to test SonarLint behaviour. Interstingly it doesn't catch that this could never run")
+
+    while ((i <= max_test) & (prime == True) & always_true): # interestingly, SonarLint doesn't catch use of & True here
+        if number % i == 0:
+            prime = False
+            break
+        i += 1
+
+    return prime
+
+# N to test
+for N in range(2,50):
+    if is_prime(N):
+        print(f"{N} is prime")
+
