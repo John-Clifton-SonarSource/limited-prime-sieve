63cf6b938515d0d37522fbd76f01c5954bb0d186
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2022-12-28 11:41:12 +0000

    Created a little file for me to try things as I learn python

---
+++ learning_python.py
@@ -0,0 +1,26 @@
+value = 5
+print(value)
+
+print(type(value))
+
+listTest = {1,3,4}
+
+print(listTest)
+
+a,b = divmod(9,4)
+
+print(a,b)
+
+marxes = ['Groucho']
+
+print(marxes.index('Groucho'))
+
+stringy = "hello there"
+
+listy = ['one','two']
+
+print(listy)
+
+del listy[0]
+
+print(listy)
